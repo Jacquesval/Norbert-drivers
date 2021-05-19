cc4b336340d1c7a10c06446e5138ee63e72dd2ef
Jacquesval <jacquesval48@gmail.com>
2021-05-19 21:46:55 +0200

    get encoder value

--- Motor.cpp
+++ Motor.cpp
@@ -1,8 +1,29 @@
-#include "Motor.h"
 #include <Arduino.h>
+#include "Motor.h"
+
+#include "include/PinChangeInt.h"
+
+Motor *instances[2] = {NULL, NULL};
+
+static void countLeft(void){
+    if (instances[LEFT] != NULL){
+        // Serial.println("count left");
+        instances[LEFT]->count();
+    }
+}
+
+static void countRight(void){
+    if (instances[RIGHT] != NULL){
+        // Serial.println("count right");
+        instances[RIGHT]->count();
+    }
+}
+
+
 
 Motor::Motor(int pwmPin, int dirPin, int encPin)
 {
+
     m_pwmPin = pwmPin;
     m_dirPin = dirPin;
     m_encPin = encPin;
@@ -11,16 +32,39 @@ Motor::Motor(int pwmPin, int dirPin, int encPin)
     m_encCount = 0;
 }
 
-int Motor::init()
-{
-    pinMode(m_pwmPin, OUTPUT);
-    pinMode(m_dirPin, OUTPUT);
-}
 
+bool Motor::init(int position = LEFT)
+{
+    if (position == LEFT || position == RIGHT){
+        instances[position] = this;
+
+        pinMode(m_pwmPin, OUTPUT);
+        pinMode(m_dirPin, OUTPUT);
+        pinMode(m_encPin, INPUT);
+        if (position == LEFT) {
+            Serial.print("attach left with"); Serial.println(m_encPin);
+            attachInterrupt (digitalPinToInterrupt(m_encPin), countLeft, CHANGE);
+        }
+        else if (position == RIGHT){
+            Serial.print("attach right with "); Serial.println(m_encPin);
+            attachPinChangeInterrupt (m_encPin, countRight, CHANGE);
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 void Motor::count()
 {
-    m_encCount ++;
+    // Serial.print(id); Serial.println(" : count");
+    if (m_pwm > 0){
+        m_encCount ++;
+    }
+    else if (m_pwm < 0) {
+        m_encCount --;
+    }
 }
 
 int Motor::setSpeed(double speed)
@@ -39,4 +83,21 @@ int Motor::setSpeed(double speed)
     
     return 1;
     
+}
+
+int Motor::getId(){
+    return id;
+}
+
+int Motor::getEncCount(){
+    return m_encCount;
+}
+
+int Motor::setEncCount(int count = 0){
+    m_encCount = count;
+    return 0;
+    }
+
+int Motor::getPwm(){
+    return m_pwm;
 }
--- Motor.h
+++ Motor.h
@@ -2,24 +2,32 @@
 #define MOTOR_H
 
 
+#define LEFT 0
+#define RIGHT 1
+
 class Motor
 {
 private:
     int m_pwmPin;
     int m_dirPin;
     int m_encPin;
+    int id;
+
 
     double m_pwm;
     int m_encCount;
+    static int counter;
 
 public:
     Motor(int pwmPin, int dirPin, int encPin);
-    int init();
+    bool init(int position);
     void count();
     int setEncCount(int count);
     int getEncCount();
+    int getPwm();
 
     int setSpeed(double speed);
+    int getId();
 };
 
 

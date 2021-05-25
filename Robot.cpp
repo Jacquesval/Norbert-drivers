577d53b220e3baab97ba15794db79153422c1030
Jacquesval <jacquesval48@gmail.com>
2021-05-25 16:35:50 +0200

    add feedback control with PID

--- Robot.cpp
+++ Robot.cpp
@@ -1,26 +1,98 @@
 #include "Robot.h"
 #include <Arduino.h>
+#include <math.h>
+#include "PID.h"
 
-#define R 1
-#define L 1
+// #include <TimerOne.h>
+#include "MsTimer2.h"
+
+
+
+Robot *robotPointer = NULL;
+
+static void controlWrapper(){
+    robotPointer->control();
+}
+
+
 
 Robot::Robot(int pwmPinRight, int dirPinRight, int encPinRight,
-          int pwmPinLeft, int dirPinLeft, int encPinLeft) : m_leftMotor(pwmPinLeft, dirPinLeft, encPinLeft), m_rightMotor(pwmPinRight, dirPinRight, encPinRight) {
+          int pwmPinLeft, int dirPinLeft, int encPinLeft) :
+          m_leftMotor(pwmPinLeft, dirPinLeft, encPinLeft),
+          m_rightMotor(pwmPinRight, dirPinRight, encPinRight),
+          m_pidL(10, 5, 0),
+          m_pidR(10, 5, 0){
 
-          }
+    m_x = 0.0;
+    m_y = 0.0;
+    m_theta = 0.0;
+    m_v = 0.0;
+    m_w = 0.0;
+    m_vRef = 200;
+    m_wRef = 0.0;
 
-int Robot::init(int stdbyPin) {
+
+    m_dEncL = 0;
+    m_dEncR = 0;
+
+    robotPointer = this;
+    m_oldEncL = 0;
+    m_oldEncR = 0;
+
+
+}
+
+Robot::~Robot(){
+    robotPointer = NULL;
+}
+
+
+int Robot::init(int controlPeriodInMs, int stdbyPin) {
     m_stdbyPin = stdbyPin;
     pinMode(m_stdbyPin, OUTPUT);
+
+    MsTimer2::set(controlPeriodInMs, controlWrapper);
+
+    m_leftMotor.init(LEFT);
+    m_rightMotor.init(RIGHT);
+    m_controlPeriodInMs = controlPeriodInMs;
+
+    return 0;
+}
+
+int Robot::run(int speed = 0){
     digitalWrite(m_stdbyPin, HIGH);
-    m_leftMotor.init();
-    m_rightMotor.init();
-    return 1;
+    m_rightMotor.setSpeed(speed);
+    m_leftMotor.setSpeed(speed);
+    MsTimer2::start();
+    return 0;
+}
+
+int Robot::stop(){
+    m_leftMotor.setSpeed(0);
+    m_rightMotor.setSpeed(0);
+    digitalWrite(m_stdbyPin, LOW);
+
+    return 0;
 }
 
 void Robot::move(double v, double w){
-    double vr = (2 * v + w * L) / (2 * R);
-    double vl = (2 * v - w * L) / (2 * R);
-    m_leftMotor.setSpeed(vl);
-    m_rightMotor.setSpeed(vr);
+    m_vRef = v;
+    m_wRef = w;
+}
+
+void Robot::getPose(double &x, double &y){
+    x = (double) m_EncL;
+    y = (double) m_vControl;
+}
+
+void Robot::control(){
+    m_EncL = m_leftMotor.getEncCount();
+    m_EncR = m_rightMotor.getEncCount();
+    m_leftMotor.setEncCount(0);
+    m_rightMotor.setEncCount(0);
+    m_vControl = m_pidL.computeOutput(m_EncL, m_vRef);
+    m_wControl = m_pidR.computeOutput(m_EncL, m_vRef);
+    m_leftMotor.setSpeed(m_vControl);
+    m_rightMotor.setSpeed(m_wControl);
 }
--- Robot.h
+++ Robot.h
@@ -2,6 +2,13 @@
 #define ROBOT_H
 
 #include "Motor.h"
+#include "PID.h"
+
+
+#define LENGTH 0.17 // length between two wheels in meter
+#define RADIUS 0.034 // radius of the wheels in meter 
+#define N 780 // encoder resolution
+#define PI 3.14
 
 class Robot
 {
@@ -9,12 +16,29 @@ private:
     Motor m_leftMotor;
     Motor m_rightMotor;
     int m_stdbyPin;
+    double m_w;
+    double m_v;
+    double m_x, m_y, m_theta;
+    double m_vRef, m_wRef;
+    int m_controlPeriodInMs;
+    double m_vControl, m_wControl;
+    int m_EncL, m_EncR;
+    int m_dEncL, m_dEncR;
+    int m_oldEncL, m_oldEncR;
+    PID m_pidR, m_pidL;
+
 
 
 public:
     Robot(int pwmPinRight, int dirPinRight, int encPinRight,
           int pwmPinLeft, int dirPinLeft, int encPinLeft);
-    int init(int stdbyPin);
+    ~Robot();
+    int init(int controlPeriodInMs, int stdbyPin);
+    int run(int speed = 0);
+    int stop();
+    void printPose();
     void move(double v, double w);
+    void getPose(double &x, double &y);
+    void control();
 };
 #endif

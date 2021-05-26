68dd41087c89177d485d8f06807d5dff59c5a25a
Jacquesval <jacquesval48@gmail.com>
2021-05-26 19:56:01 +0200

    clean code

--- Norbert-drivers.ino
+++ Norbert-drivers.ino
@@ -6,50 +6,31 @@
 #include "Robot.h"
 #include "include/Pins.h"
 
+
+
+
 ros::NodeHandle nh;
 
-
-std_msgs::Float64MultiArray debugMsg;
-ros::Publisher debug("debug", &debugMsg);
-
 Robot norbert(PWMB_RIGHT, BIN1, ENCODER_RIGHT_A_PIN,
-              PWMA_LEFT, AIN1, ENCODER_LEFT_A_PIN);
-
+            PWMA_LEFT, AIN1, ENCODER_LEFT_A_PIN);
 
 void setup()
 { 
   nh.getHardware()->setBaud(115200);
-  // Serial.begin(115200);
   pinMode(STBY_PIN, OUTPUT);
 
-  debugMsg.layout.dim = (std_msgs::MultiArrayDimension *)
-  malloc(sizeof(std_msgs::MultiArrayDimension)*2);
-  debugMsg.layout.dim[0].label = "height";
-  debugMsg.layout.dim[0].size = 2;
-  debugMsg.layout.dim[0].stride = 1;
-  debugMsg.layout.data_offset = 0;
-  debugMsg.data = (float *)malloc(sizeof(float)*4);
-  debugMsg.data_length =2;
-  debugMsg.data[0] = 0;
-  debugMsg.data[1] = 0;
-  nh.advertise(debug);
   nh.initNode();
-  delay(500);
+  delay(1000);
 
-  norbert.init(100, STBY_PIN);
+  norbert.init(100, STBY_PIN, nh);
   delay(500);
-  norbert.run(0);
-  // norbert.move(10, 0);
+  norbert.run();
 
   
 }
 
 double x,y;
 void loop(){
-  delay(500);
-  norbert.getPose(x, y) ;
-  debugMsg.data[0] = x;
-  debugMsg.data[1] = y;
-  debug.publish(&debugMsg);
+  delay(1);
   nh.spinOnce();  
 }
